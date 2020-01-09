a1553f97f227bbe05d10eb1d38641c5439912651
Adron Hall <adronhall@gmail.com>
2020-01-09 12:29:32 -0800

    Adding first commit and copyright @ DataStax.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # better-botz-ref-application
+
 This is a reference application, using the Better Botz organization domain, for building applications with the DataStax Apollo DBaaS!
--- server.py
+++ server.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_swagger import swagger
 
 app = Flask(__name__)
 
 
+@app.route("/spec")
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "My API"
+    return jsonify(swag)
+
 @app.route('/')
 def index():
     return 'Index Page'
+
